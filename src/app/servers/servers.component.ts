522b53011fa02ccbf116e48f9509ba61a33c3c4b
Eva Li <evali@truckstop.com>
2019-06-18 09:23:54 -0500

    Added comments

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,8 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // either or template/url below:
   templateUrl: './servers.component.html',
+  // adding html codes directly as template
+  // template: '<app-server></app-server><app-server></app-server>'
+
+  // you can have multiple styles
   styleUrls: ['./servers.component.css']
+
 })
 export class ServersComponent implements OnInit {
 
